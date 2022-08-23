2f7748cbf5fe39f726e5b70f283411ed1698511e
Alexander Ewetumo <trinoxf@gmail.com>
2022-08-23 04:34:43 +0000

    UPT: update simple factory

---
+++ sources/storage-factory-fcc/ExtraStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.8;
+
+import "./SimpleStorage.sol";
+
+contract ExtraStorage is SimpleStorage {
+    function store(uint256 _favoriteNumber) public override {
+        favoriteNumber = _favoriteNumber + 5;
+    }
+}
+
