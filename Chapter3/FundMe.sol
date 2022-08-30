6e4cd07b639443ade5463e24073b7fa46a848995
Alexander Ewetumo <trinoxf@gmail.com>
2022-08-30 17:03:44 +0000

    UPT: update dapp

--- Chapter3/FundMe.sol
+++ Chapter3/FundMe.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-error NotOwner();
+    error NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
