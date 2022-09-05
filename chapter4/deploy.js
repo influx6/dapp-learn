a053ebe6cf17fc9e2b713f9ef57c51d128716711
Alexander Ewetumo <trinoxf@gmail.com>
2022-09-05 23:29:43 +0000

    UPT: update factory and service

--- chapter4/deploy.js
+++ chapter4/deploy.js
@@ -1,5 +1,23 @@
-async function deploy() {
+const ethers = require("ethers");
+const fs = require("fs-extra");
 
+async function deploy() {
+    const chainEndpoint = "http://0.0.0.0:8545";
+    const provider = ethers.providers.JsonRpcProvider(chainEndpoint);
+    const wallet = ethers.Wallet("e836898c8059b584d104ad02f7e77eff83fae0caf8d742b8a428ffdae7302ff7", provider);
+
+    const contractABI = fs.readFileSync("./contracts/SimpleStorage_sol_SimpleStorage.abi");
+    const contractBinary = fs.readFileSync("./contracts/SimpleStorage_sol_SimpleStorage.bin");
+
+    const contractFactory = new ethers.ContractFactory(contractABI, contractBinary, wallet);
+
+    console.log("Deploying contract, ....");
+
+    const contract = await contractFactory.deploy();
+
+    console.log("Contract: ", contract);
+
+    
 }
 
 deploy()
