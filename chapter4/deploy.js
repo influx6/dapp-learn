93aafbb6fd1154abf690440d6d36d7b1831a6b63
Alexander Ewetumo <trinoxf@gmail.com>
2022-09-06 16:23:15 +0000

    Mov values to env file

--- chapter4/deploy.js
+++ chapter4/deploy.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
+
 const ethers = require("ethers");
 const fs = require("fs-extra");
 
 async function deploy() {
-    const chainEndpoint = "http://0.0.0.0:8545";
-    const provider = ethers.providers.JsonRpcProvider(chainEndpoint);
-    const wallet = ethers.Wallet("e836898c8059b584d104ad02f7e77eff83fae0caf8d742b8a428ffdae7302ff7", provider);
+    const provider = ethers.providers.JsonRpcProvider(process.env.CHAIN_ENDPOINT);
+    const wallet = ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
     const contractABI = fs.readFileSync("./contracts/SimpleStorage_sol_SimpleStorage.abi");
     const contractBinary = fs.readFileSync("./contracts/SimpleStorage_sol_SimpleStorage.bin");
