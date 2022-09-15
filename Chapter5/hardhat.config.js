8be013d21ba7baf707747ad9e32ca5b06cf208a9
Alexander Ewetumo <trinoxf@gmail.com>
2022-09-15 00:27:57 +0000

    UPT: add rinkeby to config

--- Chapter5/hardhat.config.js
+++ Chapter5/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks: {
+    "rinkeby": {
+      url: process.env.CHAIN_ENDPOINT,
+      chainId: process.env.CHAIN_NUMBER,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  }
 };
