37a8f5e0925c31ff5891a70180808e5989c6307f
Alexander Ewetumo <trinoxf@gmail.com>
2022-09-15 15:02:18 +0000

    UPT: Add custom localhost node with hardhat

--- Chapter5/hardhat.config.js
+++ Chapter5/hardhat.config.js
@@ -10,6 +10,10 @@ module.exports = {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
   networks: {
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+    },
     "rinkeby": {
       url: process.env.CHAIN_ENDPOINT,
       chainId: process.env.CHAIN_NUMBER,
